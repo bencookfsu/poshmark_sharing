5ce7ff8e22a5886eb0b8d7d3705bccd46729f884
korguht <me@korguht.me>
2021-03-02 11:32:18 -0500

    run.sh updates

--- run.sh
+++ run.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
+echo "STARTING: $(date)"
 SECONDS=0
-python ./share_war.py
+python3 ./share_war.py
 if (( $SECONDS > 3600 )) ; then
     let "hours=SECONDS/3600"
     let "minutes=(SECONDS%3600)/60"
     let "seconds=(SECONDS%3600)%60"
-    echo "Completed in $hours hour(s), $minutes minute(s) and $seconds second(s)"
+    echo "COMPLETE: $hours:$minutes:$seconds"
 elif (( $SECONDS > 60 )) ; then
     let "minutes=(SECONDS%3600)/60"
     let "seconds=(SECONDS%3600)%60"
-    echo "Completed in $minutes minute(s) and $seconds second(s)"
+    echo "COMPLETE: 00:$minutes:$seconds"
 else
-    echo "Completed in $SECONDS seconds"
+    echo "00:00:$SECONDS"
 fi
+echo "ENDING: $(date)"
